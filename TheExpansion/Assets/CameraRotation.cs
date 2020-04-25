c0e2efa0839c17d096f984599b2fd434beb2b910
Andreu Margarit <andreu.margarit@enti.cat>
2020-04-25 18:27:43 +0200

    Player And camera movement. Weapon gameobject put.

---
+++ TheExpansion/Assets/CameraRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    private float xRotation = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xRotation -= Input.GetAxis("Mouse Y") * 150 * Time.deltaTime;
+        if (xRotation > 45) xRotation = 45;
+        else if (xRotation < -45) xRotation = -45;
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0.0f, 0.0f);
+    }
+}
---
+++ TheExpansion/Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float yRotation =0;
+    private float speed = 5;
+    private Vector3 nextPosition = Vector3.zero;
+    RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nextPosition = transform.position;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            nextPosition -= transform.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            nextPosition += transform.right * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            nextPosition += transform.forward * Time.deltaTime * speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            nextPosition -= transform.forward * Time.deltaTime * speed;
+        }
+
+        if (Physics.Linecast(transform.position, nextPosition + (nextPosition-transform.position) * 10, out hit))
+        {
+            nextPosition = transform.position;
+        }
+
+        transform.position = nextPosition;
+
+        yRotation += Input.GetAxis("Mouse X") * 150 * Time.deltaTime;
+        
+        transform.rotation = Quaternion.Euler(0.0f, yRotation, 0.0f);
+    }
+}
