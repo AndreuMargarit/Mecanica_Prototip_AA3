e65b68ef281f7fc1e9e4a62ef2b8c57b29a43b92
Andreu Margarit <andreu.margarit@enti.cat>
2020-04-26 02:06:10 +0200

    Shot, ParticleSystem implemented

---
+++ TheExpansion/Assets/BulletManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    private float speed = 30;
+    [SerializeField] private GameObject pS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject bulletPS = Instantiate(pS, transform.position, transform.rotation);
+        bulletPS.transform.LookAt(transform.position - transform.forward, Vector3.up);
+        bulletPS.GetComponent<ParticleSystem>().Play();
+        Destroy(this.gameObject);
+    }
+}
